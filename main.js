6d8a9b8f3443db5215addbf700a7b7b1bd0a5ca0
Supatchaya Smith <tai.pakdee@gmail.com>
2020-11-19 18:41:24 -0700

    Got rid of grid divs in html and added function to make them show up with jquery.

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-$('.grid-item').on('click', function () {
-  $(this).toggleClass('toggled-gray');
-});
+
+$(document).ready(function () {
+  for (let i = 0; i < 16; i++) {
+    $('#grid-container').append('<div class="grid-item"></div>')
+  }
+  $('.grid-item').on('click', function () {
+    $(this).toggleClass('toggled-gray');
+  });
+})
